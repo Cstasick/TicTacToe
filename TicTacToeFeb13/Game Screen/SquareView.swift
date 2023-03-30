bf61cc9b86dc538bb1077fddedca6143df2b77fb
Cstasick <stasicol000@tamu.edu>
2023-03-30 13:24:14 -0700

    midterm

---
+++ TicTacToeFeb13/Game Screen/SquareView.swift
@@ -0,0 +1,37 @@
+//
+//  SquareView.swift
+//  TicTacToeFeb13
+//
+//  Created by Rajiv Mukherjee on 2/26/23.
+//
+
+import SwiftUI
+
+struct SquareView: View {
+    //to access the GameService class in this view
+    @EnvironmentObject var game:GameService
+    let index: Int
+    
+    var body: some View {
+        Button{
+            if !game.isThinking{
+                game.makeMove(at: index)
+            }// end of if
+            
+        } label: {
+            game.gameBoard[index].image
+                .resizable()
+            //midterm - change the size of square
+                .frame(width:75, height: 75)
+                .border(.primary)
+        }
+        .disabled(game.gameBoard[index].player != nil)
+    }
+}
+
+struct SquareView_Previews: PreviewProvider {
+    static var previews: some View {
+        SquareView(index: 1)
+            .environmentObject(GameService())
+    }
+}
